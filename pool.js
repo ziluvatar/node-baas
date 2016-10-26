445266518691d0eca4c0f937e74fd045f50fa198
Yamil Asusta <hello@yamilasusta.com>
2016-10-26 14:28:11 -0400

    Emit client errors

--- pool.js
+++ pool.js
@@ -24,8 +24,8 @@ BaaSPool.prototype._getClient = function (callback) {
   const self = this;
 
   self._clients
-      .filter(c => c._requestCount >= self._options.maxRequestsPerConnection || (c.stream && !c.stream.writable))
-      .forEach(c => self._killClient(c));
+    .filter(c => c._requestCount >= self._options.maxRequestsPerConnection || (c.stream && !c.stream.writable))
+    .forEach(c => self._killClient(c));
 
   if (self._openClients < self._options.maxConnections) {
     self._openClients++;
@@ -37,8 +37,13 @@ BaaSPool.prototype._getClient = function (callback) {
       callback(null, newClient);
     });
 
-    newClient.on('error', function () {
+    newClient.on('error', function (err) {
+      self.emit('error', err);
       self._killClient(newClient);
+    });
+
+    newClient.on('breaker_error', function (err) {
+      self.emit('breaker_error', err);
     });
 
     return;
